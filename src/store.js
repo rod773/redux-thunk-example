53d7d1be248916f75f29d63fba4d9eae1fb9a20d
rod7773 <rodrigoarcadio1@gmail.com>
2024-02-14 10:21:44 +0100

    update

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { legacy_createStore, applyMiddleware } from "redux";
 import { notesReducer } from "./notesReducer";
-import thunk from "redux-thunk";
+import { thunk } from "redux-thunk";
 
 export const store = legacy_createStore(notesReducer, applyMiddleware(thunk));

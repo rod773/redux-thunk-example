4ca1ddcdcde654d1764183642d962c0097940a6e
rod7773 <rodrigoarcadio1@gmail.com>
2024-02-14 09:31:18 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import { useDispatch, useSelector } from "react-redux";
+import { NewNoteInput } from "./NewNoteInput";
 
 function App() {
   const notes = useSelector((state) => state.notes);
 
   const dispatch = useDispatch();
 
+  const onAddNote = (note) => {
+    dispatch(addNote(note));
+  };
+
   return (
     <div style={{ textAlign: "center" }}>
       <h1>Redux Thunk Example</h1>
+      <NewNoteInput addNote={addNote} />
+      <br />
+      <ul>
+        {notes.map((note) => {
+          return <li key={note}>{note}</li>;
+        })}
+      </ul>
     </div>
   );
 }

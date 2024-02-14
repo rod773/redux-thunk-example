5c5b79ebf77d81843a94eee6ed47f13188e80170
rod773 <104315743+rod773@users.noreply.github.com>
2024-02-14 07:40:54 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./index.scss";
+import "bootstrap/dist/js/bootstrap.bundle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
